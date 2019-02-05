51f912b41d59104243a0ef6d13edef73166a6134
J. Singleton <jennifer.singleton@gmail.com>
2019-02-05 01:39:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
 
-  # kickstarter.css(".project-thumbnail")
+  # projects: kickstarter.css("li.project.grid_4")
   binding.pry
 end
 
