880dcd7258266c6785298f449de39adcbca949b3
J. Singleton <jennifer.singleton@gmail.com>
2019-02-05 01:16:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
-# require libraries/modules here
+require Nokogiri
 
 def create_project_hash
-  # write your code here
+  # nested hash as follows:
+      # :projects => {
+      #   :image_link => "Image Link",
+      #   :description => "Description",
+      #   :location => "Location",
+      #   :percent_funded => "Percent Funded"
+      # }
 end
